c81bc859f6578d47eaee52caa1acd41a47b8281c
Marvin GOMES VITORINO <56402327+Gomerinho@users.noreply.github.com>
2021-12-30 16:41:02 +0100

    [ADD] Back-End project configuration
    Ajout des configurations du back

---
+++ backprojetapi/src/main/java/com/backprojetapi/backprojetapi/BackprojetapiApplication.java
@@ -0,0 +1,13 @@
+package com.backprojetapi.backprojetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackprojetapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackprojetapiApplication.class, args);
+	}
+
+}
---
+++ backprojetapi/src/test/java/com/backprojetapi/backprojetapi/BackprojetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backprojetapi.backprojetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackprojetapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
